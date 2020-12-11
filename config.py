548c69689a6cdbd2abc68fd6f55ef0e2ea85d28b
christopherjpaine <christopherjpaine@gmail.com>
2020-12-11 00:11:54 +0000

    Update example

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ class Config:
 
     # Canvas size 
     canvasWidthPx = 5000
-    canvasHeightPx = 2500
+    canvasHeightPx = 5000
 
     # average size of region
-    regionSizePx = 50
+    regionSizePx = 200
 
     # Regularity of region Lower = more irregular.
     # canvasSize/regionSize is the most reasonable minimum regularity otherwise the 
     # noise is larger than the canvas itself. Very regular (<1px irregularity)
     # is acheived around canvasSize*regionSize
-    regionRegularity = 5
+    regionRegularity = 0.5
 
     ## Fade and Coloring
 
@@ -25,19 +25,19 @@ class Config:
     # in the fade region.
     # example input formats -> https://www.w3.org/TR/SVGColor12/#sRGBcolor
     # predef color list -> https://www.december.com/html/spec/colorsvg.html
-    colorList = ['#FFFFFF', '#CCCCCC', '#808080', '#606060', '#404040', '#202020']
+    colorList = ['#FFFFFF', '#808080', '#606060', '#202020']
 
     # Enabled Fade Coloring
     fadeEnabled = True
 
     # Fade diffusion, how far outside of its region do we expect to see a given color
     # From 0 to 2 seems to be the reasonable values.
-    fadeDiffusion = 0.3
+    fadeDiffusion = 0.4
 
     # Size of fade region relative to the size of the whole image 
     # 1 = across full image, 0 = very centre only
     # Can also be thought of as the probability of the fade colors appearing.
-    fadeSize = 0.18
+    fadeSize = 0.5
 
 
 
